6f3962ebddfb2bb3f72063ce519b2ba3cf7f06c4
Rusty <rjohnson5016@gmail.com>
2022-04-08 19:50:26 -0400

    Saving qrcode to file

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const { Client, Intents, MessageEmbed } = require('discord.js');
 const { token } = require('./config.json');
 const QRCode = require('qrcode');
+const fs = require('fs-extra');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+const qrcode = new QRCode.toDataURL('www.google.com', function (error, code) {
+	if (error) {
+		console.log(error);
+	}
+	const matches = code.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+	const qrcodeImage = {};
+
+	qrcodeImage.type = matches[1];
+	qrcodeImage.data = Buffer.from(matches[2], 'base64');
+	fs.writeFileSync('qrcode.png', qrcodeImage.data);
+});
+
+if (qrcode);
 client.once('ready', () => {
 	console.log('Ready');
 });
 
 client.on('interactionCreate', (interaction) => {
 	const embed = new MessageEmbed().setTitle('Hi, I dog!');
-	qrcode = QRCode.toDataURL('www.google.com', function (error, code) {
-		if (error) {
-			console.log(error);
-		}
-		return code;
-	});
 
 	if (!interaction.isCommand()) return;
 
@@ -24,7 +32,7 @@ client.on('interactionCreate', (interaction) => {
 	if (commandName === 'generate-qr-code') {
 		interaction.channel.send({
 			embeds: [embed],
-			files: ['./dog_pic.jpeg'],
+			files: ['qrcode.png'],
 		});
 	}
 });
